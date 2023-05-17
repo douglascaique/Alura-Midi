f5d2db3117fd239ee17f0660ecabff326019afed
douglascaique <60799530+douglascaique@users.noreply.github.com>
2023-05-17 16:05:52 -0300

    Passando para teclado

--- main.js
+++ main.js
@@ -1,6 +1,36 @@
-//pom
-function tocaSomPom () {
-    document.querySelector('#som_tecla_pom').play();
+function tocaSom (idAudio) {
+
+    document.querySelector(idAudio).play();
 }
 
-document.querySelectorAll
+
+
+
+// referencias, usadas com base no valor que vao receber e no que devem guardar 
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+let contador = 0;
+
+while(contador < listaDeTeclas.length){
+
+    const tecla = listaDeTeclas[contador];
+
+    const instrumento = tecla.classList[1];
+    
+    console.log(instrumento);
+
+    const idAudio = `#som_${instrumento}`;
+    console.log(idAudio);
+
+
+    
+    tecla.onclick = function(){
+        tocaSom(idAudio);
+    }
+
+    contador++;
+
+    console.log(contador);
+}
+
+
